f97cfa871250f22973466f3c514de4f184b731e1
ousttrue <ousttrue@gmail.com>
2023-04-08 21:48:35 +0900

    splitter

--- src/docks/json_dock.cpp
+++ src/docks/json_dock.cpp
@@ -1,5 +1,7 @@
 #include "json_dock.h"
 #include <imgui.h>
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include <imgui_internal.h>
 #include <ranges>
 #include <sstream>
 
@@ -88,6 +90,37 @@ struct JsonDockImpl
   }
 };
 
+bool
+Splitter(bool split_vertically,
+         float thickness,
+         float* size1,
+         float* size2,
+         float min_size1,
+         float min_size2,
+         float splitter_long_axis_size = -1.0f)
+{
+  using namespace ImGui;
+  ImGuiContext& g = *GImGui;
+  ImGuiWindow* window = g.CurrentWindow;
+  ImGuiID id = window->GetID("##Splitter");
+  ImRect bb;
+  bb.Min = window->DC.CursorPos +
+           (split_vertically ? ImVec2(*size1, 0.0f) : ImVec2(0.0f, *size1));
+  bb.Max = bb.Min + CalcItemSize(split_vertically
+                                   ? ImVec2(thickness, splitter_long_axis_size)
+                                   : ImVec2(splitter_long_axis_size, thickness),
+                                 0.0f,
+                                 0.0f);
+  return SplitterBehavior(bb,
+                          id,
+                          split_vertically ? ImGuiAxis_X : ImGuiAxis_Y,
+                          size1,
+                          size2,
+                          min_size1,
+                          min_size2,
+                          0.0f);
+}
+
 void
 JsonDock::Create(const AddDockFunc& addDock,
                  std::string_view title,
@@ -101,9 +134,29 @@ JsonDock::Create(const AddDockFunc& addDock,
   };
   auto leave = []() { ImGui::TreePop(); };
 
-  addDock(Dock(title, [scene, enter, leave, indent]() {
-    ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, indent);
-    scene->TraverseJson(enter, leave);
-    ImGui::PopStyleVar();
-  }));
+  addDock(Dock(
+    title,
+    [scene, enter, leave, indent](const char* title, bool* p_open) mutable {
+      ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 0, 0 });
+      if (ImGui::Begin(title, p_open)) {
+
+        auto size = ImGui::GetContentRegionAvail();
+        static float f = 300;
+        static float s = 300;
+
+        ::Splitter(false, 5, &f, &s, 8, 8);
+        if (ImGui::BeginChild("##split-first", { size.x, f })) {
+          ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, indent);
+          scene->TraverseJson(enter, leave);
+          ImGui::PopStyleVar();
+        }
+        ImGui::EndChild();
+
+        if (ImGui::BeginChild("##split-second", { size.x, s })) {
+        }
+        ImGui::EndChild();
+      }
+      ImGui::End();
+      ImGui::PopStyleVar();
+    }));
 }
