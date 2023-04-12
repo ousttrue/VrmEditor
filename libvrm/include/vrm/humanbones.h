c3c3962d7d5e4261d92c64f99f1ccb7261ad3623
ousttrue <ousttrue@gmail.com>
2023-04-13 01:59:35 +0900

    enum 0

--- libvrm/include/vrm/humanbones.h
+++ libvrm/include/vrm/humanbones.h
@@ -5,12 +5,15 @@
 
 namespace vrm {
 
-enum class VrmVersion {
+enum class VrmVersion
+{
   _0_x,
   _1_0,
 };
 
-enum class HumanBones {
+enum class HumanBones
+{
+  unknown,
   hips,
   spine,
   chest,
@@ -71,81 +74,83 @@ enum class HumanBones {
   rightLittleDistal,
   VRM_BONE_COUNT,
 };
-constexpr const char *HumanBonesNames[] = {
-    "hips",
-    "spine",
-    "chest",
-    "upperChest",
-    "neck",
-    "head",
-    "leftEye",
-    "rightEye",
-    "jaw",
-    "leftShoulder",
-    "leftUpperArm",
-    "leftLowerArm",
-    "leftHand",
-    "rightShoulder",
-    "rightUpperArm",
-    "rightLowerArm",
-    "rightHand",
-    "leftUpperLeg",
-    "leftLowerLeg",
-    "leftFoot",
-    "leftToes",
-    "rightUpperLeg",
-    "rightLowerLeg",
-    "rightFoot",
-    "rightToes",
-    "leftThumbMetacarpal",
-    "leftThumbProximal",
-    "leftThumbDistal",
-    "leftIndexProximal",
-    "leftIndexIntermediate",
-    "leftIndexDistal",
-    "leftMiddleProximal",
-    "leftMiddleIntermediate",
-    "leftMiddleDistal",
-    "leftRingProximal",
-    "leftRingIntermediate",
-    "leftRingDistal",
-    "leftLittleProximal",
-    "leftLittleIntermediate",
-    "leftLittleDistal",
-    "rightThumbMetacarpal",
-    "rightThumbProximal",
-    "rightThumbDistal",
-    "rightIndexProximal",
-    "rightIndexIntermediate",
-    "rightIndexDistal",
-    "rightMiddleProximal",
-    "rightMiddleIntermediate",
-    "rightMiddleDistal",
-    "rightRingProximal",
-    "rightRingIntermediate",
-    "rightRingDistal",
-    "rightLittleProximal",
-    "rightLittleIntermediate",
-    "rightLittleDistal",
+constexpr const char* HumanBonesNames[] = {
+  "unknown",
+  "hips",
+  "spine",
+  "chest",
+  "upperChest",
+  "neck",
+  "head",
+  "leftEye",
+  "rightEye",
+  "jaw",
+  "leftShoulder",
+  "leftUpperArm",
+  "leftLowerArm",
+  "leftHand",
+  "rightShoulder",
+  "rightUpperArm",
+  "rightLowerArm",
+  "rightHand",
+  "leftUpperLeg",
+  "leftLowerLeg",
+  "leftFoot",
+  "leftToes",
+  "rightUpperLeg",
+  "rightLowerLeg",
+  "rightFoot",
+  "rightToes",
+  "leftThumbMetacarpal",
+  "leftThumbProximal",
+  "leftThumbDistal",
+  "leftIndexProximal",
+  "leftIndexIntermediate",
+  "leftIndexDistal",
+  "leftMiddleProximal",
+  "leftMiddleIntermediate",
+  "leftMiddleDistal",
+  "leftRingProximal",
+  "leftRingIntermediate",
+  "leftRingDistal",
+  "leftLittleProximal",
+  "leftLittleIntermediate",
+  "leftLittleDistal",
+  "rightThumbMetacarpal",
+  "rightThumbProximal",
+  "rightThumbDistal",
+  "rightIndexProximal",
+  "rightIndexIntermediate",
+  "rightIndexDistal",
+  "rightMiddleProximal",
+  "rightMiddleIntermediate",
+  "rightMiddleDistal",
+  "rightRingProximal",
+  "rightRingIntermediate",
+  "rightRingDistal",
+  "rightLittleProximal",
+  "rightLittleIntermediate",
+  "rightLittleDistal",
 };
 
-inline std::optional<HumanBones> HumanBoneFromName(std::string_view boneName,
-                                                   VrmVersion version) {
+inline std::optional<HumanBones>
+HumanBoneFromName(std::string_view boneName, VrmVersion version)
+{
   switch (version) {
-  case VrmVersion::_0_x:
-    if (boneName == "leftThumbProximal") {
-      return HumanBones::leftThumbMetacarpal;
-    } else if (boneName == "leftThumbIntermediate") {
-      return HumanBones::leftThumbProximal;
-    } else if (boneName == "rightThumbProximal") {
-      return HumanBones::rightThumbMetacarpal;
-    } else if (boneName == "rightThumbIntermediate") {
-      return HumanBones::rightThumbProximal;
-    }
-    break;
+    case VrmVersion::_0_x:
+      if (boneName == "leftThumbProximal") {
+        return HumanBones::leftThumbMetacarpal;
+      } else if (boneName == "leftThumbIntermediate") {
+        return HumanBones::leftThumbProximal;
+      } else if (boneName == "rightThumbProximal") {
+        return HumanBones::rightThumbMetacarpal;
+      } else if (boneName == "rightThumbIntermediate") {
+        return HumanBones::rightThumbProximal;
+      }
+      break;
 
-  default:
-    break;
+    default:
+      break;
   }
 
   for (int i = 0; i < std::size(HumanBonesNames); ++i) {
@@ -157,7 +162,9 @@ inline std::optional<HumanBones> HumanBoneFromName(std::string_view boneName,
   return {};
 }
 
-inline const char *HumanBoneToName(HumanBones bone) {
+inline const char*
+HumanBoneToName(HumanBones bone)
+{
   return HumanBonesNames[(int)bone];
 }
 
