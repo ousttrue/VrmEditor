867904cb4f408857a631663a7f42232d3d61139e
ousttrue <ousttrue@gmail.com>
2023-04-13 11:00:22 +0900

    WIP MakeSkeleton, MakeFrame

--- libvrm/include/vrm/srht_update.h
+++ libvrm/include/vrm/srht_update.h
@@ -1,13 +1,24 @@
 #pragma once
-#include "scene.h"
 #include <DirectXMath.h>
 #include <memory>
 #include <span>
 #include <stdint.h>
+#include <vector>
+#include <vrm/scene.h>
 
 namespace srht {
 void
 UpdateScene(const std::shared_ptr<gltf::Scene>& scene,
             std::vector<DirectX::XMFLOAT4X4>& instances,
             std::span<const uint8_t> data);
+
+void
+MakeSkeleton(uint32_t skeletonId,
+             const std::shared_ptr<gltf::Node>& root,
+             std::vector<uint8_t>& out);
+
+void
+MakeFrame(uint32_t skeletonId,
+          const std::shared_ptr<gltf::Node>& root,
+          std::vector<uint8_t>& out);
 }
