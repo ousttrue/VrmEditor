a35afbb09fb9b5e0b8906d78529216cb8079f479
ousttrue <ousttrue@gmail.com>
2023-06-07 20:54:52 +0900

    fix build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,3 +1,4 @@
 meson setup builddir --prefix "$(pwd)/vrmeditor" --buildtype release -Dcpp_std=c++latest -Dexecutables=true
 meson install -C builddir --tags runtime
 
+Compress-Archive -Path "$(pwd)/vrmeditor" -DestinationPath "vrmeditor.zip"
