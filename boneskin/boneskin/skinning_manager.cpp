e36ca5189059395de07bb1922938aaf49cb49df2
ousttrue <ousttrue@gmail.com>
2023-06-18 00:05:58 +0900

    ApplyMorphTargetAndSkinning

--- boneskin/boneskin/skinning_manager.cpp
+++ boneskin/boneskin/skinning_manager.cpp
@@ -298,6 +298,77 @@ SkinningManager::GetOrCreaeSkin(const gltfjson::Root& root,
   }
 }
 
+static void
+ApplySkinning(Vertex* dst,
+              const Vertex& src,
+              float w,
+              std::span<const DirectX::XMFLOAT4X4> matrices,
+              uint16_t matrixIndex)
+{
+  if (w > 0) {
+    if (matrixIndex < matrices.size()) {
+      DirectX::XMFLOAT3 store;
+      {
+        auto pos = DirectX::XMLoadFloat3(&src.Position);
+        auto newPos = DirectX::XMVector3Transform(
+          pos, DirectX::XMLoadFloat4x4(&matrices[matrixIndex]));
+        DirectX::XMStoreFloat3(&store, newPos);
+        dst->Position += (store * w);
+      }
+      {
+        auto normal = DirectX::XMLoadFloat3(&src.Normal);
+        auto newNormal = DirectX::XMVector3Transform(
+          normal, DirectX::XMLoadFloat4x4(&matrices[matrixIndex]));
+        DirectX::XMStoreFloat3(&store, newNormal);
+        dst->Normal += (store * w);
+      }
+    } else {
+      // error
+    }
+  }
+}
+
+static void
+ApplyMorphTargetAndSkinning(
+  DeformedMesh& deformed,
+  const BaseMesh& mesh,
+  const std::unordered_map<uint32_t, float>& morphMap,
+  std::span<const DirectX::XMFLOAT4X4> skinningMatrices)
+{
+  // clear & apply morph target
+  deformed.Vertices.clear();
+  for (int i = 0; i < mesh.m_vertices.size(); ++i) {
+    auto v = mesh.m_vertices[i];
+    for (int j = 0; j < mesh.m_morphTargets.size(); ++j) {
+      auto& morphtarget = mesh.m_morphTargets[j];
+      auto found = morphMap.find(j);
+      if (found != morphMap.end()) {
+        v.Position += morphtarget->Vertices[i].position * found->second;
+      }
+    }
+    deformed.Vertices.push_back(v);
+  }
+
+  // calc skinning
+  if (skinningMatrices.size()) {
+    for (int i = 0; i < mesh.m_vertices.size(); ++i) {
+      auto src = deformed.Vertices[i];
+      auto& dst = deformed.Vertices[i];
+      dst.Position = { 0, 0, 0 };
+      dst.Normal = { 0, 0, 0 };
+      auto binding = mesh.m_bindings[i];
+      if (auto w = binding.Weights.x)
+        ApplySkinning(&dst, src, w, skinningMatrices, binding.Joints.X);
+      if (auto w = binding.Weights.y)
+        ApplySkinning(&dst, src, w, skinningMatrices, binding.Joints.Y);
+      if (auto w = binding.Weights.z)
+        ApplySkinning(&dst, src, w, skinningMatrices, binding.Joints.Z);
+      if (auto w = binding.Weights.w)
+        ApplySkinning(&dst, src, w, skinningMatrices, binding.Joints.W);
+    }
+  }
+}
+
 std::span<const NodeMesh>
 SkinningManager::ProcessSkin(const gltfjson::Root& root,
                              const gltfjson::Bin& bin,
@@ -348,8 +419,8 @@ SkinningManager::ProcessSkin(const gltfjson::Root& root,
         if (deformed->Vertices.size()) {
           // apply morphtarget & skinning
           // rmt_ScopedCPUSample(SkinningApply, 0);
-          deformed->ApplyMorphTargetAndSkinning(
-            *baseMesh, drawables[i].MorphMap, skinningMatrices);
+          ApplyMorphTargetAndSkinning(
+            *deformed, *baseMesh, drawable.MorphMap, skinningMatrices);
         }
       }
     }
